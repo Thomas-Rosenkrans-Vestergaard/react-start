d1200768bdc3e2f700f714666c53d93615782a0e
Thomas Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2019-01-12 23:38:42 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="container">
+        <header>
+          <h1>Title</h1>
+          <nav>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </nav>
+        </header>
+        <main></main>
+      </div>
+    );
+  }
+}
+
+export default App;
