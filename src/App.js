00e447eed35962b0be12968b1e9618c917e1d5f3
Thomas-Rosenkrans-Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2019-01-14 14:15:26 +0100

    added react-toastr to the project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { HashRouter as Router, NavLink, Route, Switch } from "react-router-dom";
+import { ToastContainer, ToastMessageAnimated } from "react-toastr";
 
 import HomePage from "./pages/HomePage";
 import FourZeroFour from "./pages/FourZeroFour";
@@ -7,10 +8,18 @@ import FourZeroFour from "./pages/FourZeroFour";
 import './App.css';
 
 class App extends Component {
+  
+  toastContainer = null;
+  
   render() {
     return (
       <Router>
         <div>
+          <ToastContainer
+            ref={ref => this.toastContainer = ref}
+            className="toast-top-right"
+            toastMessageFactory={React.createFactory(ToastMessageAnimated)}
+          />
           <header>
             <h1>Title</h1>
             <nav>
@@ -21,13 +30,17 @@ class App extends Component {
           </header>
           <main>
             <Switch>
-              <Route exact path="/" render={router => <HomePage router={router} />} />
-              <Route render={router => <FourZeroFour router={router} />} />
+              <Route exact path="/" render={router => <HomePage router={router} toastFactory={this.toastFactory} />} />
+              <Route render={router => <FourZeroFour router={router} />} toastFactory={this.toastFactory} />
             </Switch>
           </main>
         </div>
       </Router>
     );
+  }
+
+  toastFactory = () => {
+    return this.toastContainer;
   }
 }
 
